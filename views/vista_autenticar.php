afc46ec8f5d8353704d1b482c6de1810754a465e
Oscar Castro <alexcaxtro@gmail.com>
2023-06-23 15:34:22 -0400

    Subiendo boton de logout

--- views/vista_autenticar.php
+++ views/vista_autenticar.php
@@ -29,14 +29,23 @@
             <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
           </svg>
         </div>
-        <div class="col-11">
+        <div class="col-10">
           <div class="h-50" >
             <div class="container">
               <h2 class="display-6" style="color: white;">Pedro Pérez</h2>
               <h5 style="color: white;">Rut: 11.111.111-1</h5>
               <div class="d-flex p-1 "></div> 
             </div>    
+        </div> 
         </div>
+        <div class="col-1 .mt-2">
+        <a href="http://www.hospitalcurico.cl/web/index.php" id="enlace">
+          <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="whiteColor" class="bi bi-box-arrow-in-right filter-green" viewBox="0 0 18 18">
+            <path fill="#fdfefe" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z"/>
+            <path fill="#fdfefe" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+          </svg>
+          <h6 style="color: white">Cerrar sesión</h6>
+          </a>
         </div>
       </div>
     </div>
